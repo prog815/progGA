75b8b3465287541be6edca45ec294990ddb6b528
prog815 <eavprog@gmail.com>
2020-07-31 22:02:19 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-import abscur
+import .
 
 setup(
     name='progGA',
-    version='0.0.2',
+    version='0.0.3',
     description='Библиотека генетических алгоритмов',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
