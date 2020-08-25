b018338b096709af03fcb48a89fb3071d711e586
prog815 <eavprog@gmail.com>
2020-08-25 17:34:09 +0500

    Update and rename list_genetic.py to lib/__init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,12 @@
 class ListGenetic:
-    
+    """
+    Класс генетической оптимизации для списочных параметров
+    Аргументы конструктора:
+        pop_size - размер популяции
+        mutate_koef - коэффициент мутации
+        quality_method - метод расчета качества
+        diaps - перечисление параметров со списками значений
+    """
     _pop = None
     _diaps = None
     _quality = None
@@ -8,6 +15,9 @@ class ListGenetic:
     _quality_method = None
 
     def __init__(self,pop_size=100,mutate_koef=0.01,quality_method=None,**diaps):
+        """
+        
+        """
         import numpy as np
         
         self._diaps = diaps
@@ -19,6 +29,11 @@ class ListGenetic:
         self._quality_method = quality_method
         
     def fit(self,epochs=10):
+        """
+        Запуск генетической оптимизации.
+        Аргументы:
+            epochs - кол-во эпох оптимизации
+        """
         import numpy as np
         
         for ep in range(epochs):
@@ -88,12 +103,59 @@ class ListGenetic:
         return new_genom
 
     def plot_hist_new(self,params=('quality',)):
+        """
+        Вывод истории оптимизации
+        Аргументы:
+            params - список параметров для вывода
+        """
         import matplotlib.pyplot as plt
         import numpy as np
         
-        for param in params:
-            v = [h['new'][param] for h in self._hist]
-            plt.plot(v)
-            plt.title(param)
-            plt.show()
+        def getQM(data,N=20):
+            L = len(data)
+            dN = L//N
+            gl = L//10
+            x = list(range(dN,L,dN))
+            if x[-1] != L-1:
+                x.append(L-1)
+            q1 = np.array(x,dtype=float)
+            m = np.array(x,dtype=float)
+            q2 = np.array(x,dtype=float)
+            for i in range(len(x)):
+                q1[i],m[i],q2[i] = np.quantile(data[(max(0,x[i]-gl)):(x[i]+1)],(0.1,0.5,0.9))
+            return x,q1,m,q2
 
+        for param in params:
+            plt.figure(figsize=(10,6))
+            v = [h['new'][param] for h in self._hist]
+            ind = list(range(0,len(v),max(1,int(np.ceil(len(v)/500)))))
+            plt.plot(ind,np.array(v)[ind],'y.',label='<'+param+'>')
+            x,q1,m,q2 = getQM(v)
+            plt.plot(x,q2,':',label='q2')
+            plt.plot(x,m,'*k-',label='m')
+            plt.plot(x,q1,':',label='q1')
+            plt.title('параметр <' + param + '>')
+            plt.legend()
+            plt.show()
+    
+    def getBestParams(self,method='pop_mean',**kwargs):
+        """
+        Выдача лучших параметров
+        Аргументы:
+            method - метод извлечения
+                варианты:
+                - pop_mean - среднее по популяции
+                - pop_q - серединный квантиль по популяции
+                
+        """
+        import numpy as np
+
+        params = list(self._diaps.keys())
+
+        if method == 'pop_mean':
+            return self._get_gen_params(self._pop.mean(axis=0))
+        
+        if method == 'pop_q':
+            return self._get_gen_params(np.quantile( self._pop,0.5,axis=0))
+
+        raise Exception('неверный метод')
